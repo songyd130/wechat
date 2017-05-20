e739f243aa55f32db12a911bd70541a1b1b72472
songyd130 <songyd130@163.com>
2017-05-21 00:52:21 +0800

    fix bug

--- app/Http/Controllers/Backend/AuthController.php
+++ app/Http/Controllers/Backend/AuthController.php
@@ -27,7 +27,7 @@ class AuthController extends Controller
 
         return Validator::make($data, [
             'name' => 'required|max:255',
-            'email' => 'required|email|max:255|unique:admins',
+            'email' => 'required|email|max:255|unique:admin_users',
             'password' => 'required|confirmed|min:6',
         ]);
 
